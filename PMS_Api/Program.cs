5f9d7fb8d0b08be137691378fcf8448b63a98fb9
Pon Chandra Prasath <chandran.aathi@live.in>
2023-10-02 20:39:21 +0530

    Add project files.

---
+++ PMS_Api/Program.cs
@@ -0,0 +1,35 @@
+﻿using PMS_Library.Models.DataModels;
+using Microsoft.EntityFrameworkCore;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddDbContext<PMS_Entities>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DatabaseConnection")));
+
+
+// Add services to the container.
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+//builder.Services.AddScoped<PMS_Entities, PMS_Entities>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+	app.UseSwagger();
+	app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ PMS_Maui/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PMS_Maui
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
